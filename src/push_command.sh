d5e7ff8bc21631022a51c4703b458bec7d10771b
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2025-04-06 14:44:30 +0900

    create push_opts for call push_tfstate make compact.

--- src/push_command.sh
+++ src/push_command.sh
@@ -1,22 +1,32 @@
+local -a push_opts=()
+push_opts+=("${args[--force]}")
+push_opts+=("${args[--ignore-remote-version]}")
+
 if [[ -n "${args[--all]}" ]]; then
   local exit_code
   local -A results=()
   for f in migrates/*.tfstate ; do
+    # check coresponding tfvars is exists.
+    if [[ -n "$(validate_tfstate "$f")" ]] ; then
+      continue
+    fi
+  
     set +e
-    push_tfstate "$f" "${args[--force]}" "${args[--ignore-remote-version]}"
+    push_tfstate "$f" "${push_opts[@]}"
     exit_code="$?"
     set -e
+
     case "$exit_code" in
-      0) results[$f]="ok" ;;
-      1) results[$f]="error" ;;
-      2) results[$f]="error, the corresponding tfvars file is missing." ;;
-      *) results[$f]="unexpected exit code."
+      0) results["$f"]="ok" ;;
+      1) results["$f"]="error" ;;
+      *) results["$f"]="unexpected exit code."
     esac
   done
+
   for f in "${!results[@]}" ; do
-    echo "$f : ${results[$f]}"
+    echo "$f : ${results["$f"]}"
   done 
 elif [[ -n "${args[file]}" ]] ; then
-  push_tfstate "${args[file]}" "${args[--force]}" "${args[--ignore-remote-version]}"
+  push_tfstate "${args[file]}" "${push_opts[@]}"
 fi
 
