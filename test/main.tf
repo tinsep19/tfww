1c9a571c8742fae6465e94bf675149cc067d4b81
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2025-04-26 20:16:35 +0900

    WIP. divide testcases.
    test/approve describe how-to
    Add testcase boilerplate scripts
    make clean use git clean

--- test/main.tf
+++
@@ -1,6 +0,0 @@
-variable "input-1" { type = string }
-resource "local_file" "file" {
-  content = "${var.input-1}"
-  filename = "out/${terraform.workspace}/content.txt"
-}
-output "output-1" { value = local_file.file.content_sha256}
